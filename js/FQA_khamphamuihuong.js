56296baa7e33dbf2c534f8d8fbfcc570f10b378c
XuanYen <xuanyenbentre@gmail.com>
2021-10-15 19:13:48 +0700

    add animation

--- js/FQA_khamphamuihuong.js
+++ js/FQA_khamphamuihuong.js
@@ -11,8 +11,13 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
 }
 function ShowTab(event) {
+    console.log(tabHeaders)
     for (var i = 0; i < tabHeaders.length; i++) {
-        tabHeaders[i].className = "";
+        if (tabHeaders[i].outerText === event.target.outerText) {
+            tabHeaders[i].classList.add("selected");
+        } else {
+            tabHeaders[i].classList.remove('selected');
+        }
     }
-    event.target.className = "selected";
+
 }
